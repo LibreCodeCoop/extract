a8704f85f044f9368da78545046c409ccceb1644
David Lima <antdavidlima@gmail.com>
2025-09-08 20:38:37 -0300

    feat: implement reuse action
    Signed-off-by: David Lima <antdavidlima@gmail.com>

--- lib/Controller/ExtractionController.php
+++ lib/Controller/ExtractionController.php
@@ -2,6 +2,11 @@
 
 namespace OCA\Extract\Controller;
 
+/**
+ * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 // Only in order to access Filesystem::isFileBlacklisted().
 use OC\Files\Filesystem;
 use OCA\Extract\ResponseDefinitions;
--- src/actions/extract-action.ts
+++ src/actions/extract-action.ts
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import type { Node, View } from '@nextcloud/files'
 
 import ArchiveArrowUpSvg from '@mdi/svg/svg/archive-arrow-up.svg?raw'
