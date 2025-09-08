4e956e53b489785a6f5984200e68a7cde75afa12
David Lima <antdavidlima@gmail.com>
2025-09-08 19:22:59 -0300

    feat: implement lint stylelint action
    Signed-off-by: David Lima <antdavidlima@gmail.com>

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,6 +2,6 @@
  * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import stylelintConfig from '@nextcloud/stylelint-config'
+const stylelintConfig = require('@nextcloud/stylelint-config')
 
-export { stylelintConfig }
+module.exports = stylelintConfig
